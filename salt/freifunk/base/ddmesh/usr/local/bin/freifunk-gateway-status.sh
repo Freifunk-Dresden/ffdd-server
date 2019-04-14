1da6aabff9b348f41f8d9838925b0d4efafd89b5
cremesk <ennik@envs.net>
2019-04-14 20:57:25 +0200

    gateway-status.sh fix vpn else

--- salt/freifunk/base/ddmesh/usr/local/bin/freifunk-gateway-status.sh
+++ salt/freifunk/base/ddmesh/usr/local/bin/freifunk-gateway-status.sh
@@ -2,7 +2,7 @@
 ### This file managed by Salt, do not edit by hand! ###
 
 ip="$(curl --interface vpn0 -s whois.envs.net)"
-[ -z "$ip" ] && ip="$(curl --interface vpn0 -sL whois.envs.net)"
+[ -z "$ip" ] && ip="$(curl --interface vpn1 -sL whois.envs.net)"
 
 if [ -n "$ip" ]; then
 	country=$(whois "$ip" | sed -n '/^country:/s#.*[: 	]##p')
