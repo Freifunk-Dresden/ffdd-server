b8816913f1cba3946a1a68c70aa041e5cbf5a8cf
creme <creme@envs.net>
2019-10-25 20:15:04 +0200

    update fastd

--- salt/freifunk/base/fastd/compiled-tools/fastd/build.sh
+++ salt/freifunk/base/fastd/compiled-tools/fastd/build.sh
@@ -18,12 +18,15 @@
 #libuecc_rev=bb4fcb93282ca2c3440294683c88e8d54a1278e0
 
 # fastd v18+ (devel)
-fastd_rev=3995adf7882a43d4c7c206a1c5335e3fdbc9c529
+#fastd_rev=3995adf7882a43d4c7c206a1c5335e3fdbc9c529
+#libuecc_rev=7c9a6f6af088d0764e792cf849e553d7f55ff99e
+# 2019
+fastd_rev=7d9f90ebe9dd3a7f6af1fbb97d8cc228fb0fded4
 libuecc_rev=7c9a6f6af088d0764e792cf849e553d7f55ff99e
+
 
 build_libuecc()
 {
-		
 	# make sub shell to avoid extra calles to cd ..
 	(
 		rm -rf libuecc
@@ -49,7 +52,7 @@ build_libuecc()
 build_fastd()
 {
 	(
-		#-DCMAKE_BUILD_TYPE:STRING=MINSIZEREL 
+		#-DCMAKE_BUILD_TYPE:STRING=MINSIZEREL
 
 		CMAKE_OPTIONS=" \
 		-DCMAKE_BUILD_TYPE=RELEASE \
@@ -83,13 +86,13 @@ build_fastd()
 			rev=$(git -C fastd log -1 | sed -n '/^commit/s#commit ##p')
 			tar czf fastd-$rev.tgz fastd
 		fi
-	
+
 		patch --directory=fastd -p0 < urandom.patch
-	
+
 		cd fastd
 		mkdir build
 		cd build
-		cmake ../ $CMAKE_OPTIONS 
+		cmake ../ $CMAKE_OPTIONS
 		#cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -DENABLE_LIBSODIUM:BOOL=FALSE -DENABLE_LTO:BOOL=FALSE -DWITH_CAPABILITIES:BOOL=FALSE
 		make
 		strip src/fastd
