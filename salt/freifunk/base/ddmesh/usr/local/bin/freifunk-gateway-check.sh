85c8fd49036e065d8b509817bb31ebd4badf5e3a
creme <creme@envs.net>
2019-11-05 15:38:14 +0100

    change gw-check ips again

--- salt/freifunk/base/ddmesh/usr/local/bin/freifunk-gateway-check.sh
+++ salt/freifunk/base/ddmesh/usr/local/bin/freifunk-gateway-check.sh
@@ -55,8 +55,8 @@ done
 $DEBUG && printf '%s\n' "start"
 
 #dont use vpn server (or any openvpn server), it could interrupt connection
-# cloudflare, google, freifunk-dresden.de, www.dresden.de
-ping_hosts="1.1.1.1 8.8.8.8 9.9.9.9 89.163.140.199 194.49.19.111"
+# cloudflare, google 2x, freifunk-dresden.de, vpn1.freifunk-dresden.de, vpn5.freifunk-dresden.de vpn6.freifunk-dresden.de
+ping_hosts="1.1.1.1 8.8.8.8 9.9.9.9 89.163.140.199 178.63.61.147 5.45.106.241 81.6.46.11"
 #process max 3 user ping
 #cfg_ping="$(uci -q get ddmesh.network.gateway_check_ping)"
 #gw_ping="$(echo "$cfg_ping" | sed 's#[ ,;/	]\+# #g' | cut -d' ' -f1-3 ) $ping_hosts"
