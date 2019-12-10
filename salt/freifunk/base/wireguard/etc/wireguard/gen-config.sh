5be444704d7fe93babf439cf693522496496c081
creme <creme@envs.net>
2019-12-10 14:08:32 +0100

    wireguard: small preup and predown changes

--- salt/freifunk/base/wireguard/etc/wireguard/gen-config.sh
+++ salt/freifunk/base/wireguard/etc/wireguard/gen-config.sh
@@ -90,8 +90,8 @@ Address = $ipv4/32
 TABLE = off
 
 PreUp = echo "forwarders { ${WG_IF_DNS//,/;}; };" > /etc/bind/vpn.forwarder.$vpn
-PostUp = ip route add default dev $vpn table gateway_pool metric $metric; iptables -w -t nat -A POSTROUTING -o $vpn -j SNAT --to-source $ipv4; freifunk-gateway-check.sh
-PreDown = ip route del default dev $vpn table gateway_pool metric $metric ; iptables -w -t nat -D POSTROUTING -o $vpn -j SNAT --to-source $ipv4 ; ip link delete $vpn ; freifunk-gateway-check.sh
+PostUp = ip route add default dev $vpn table gateway_pool metric $metric ; iptables -w -t nat -A POSTROUTING -o $vpn -j SNAT --to-source $ipv4 ; freifunk-gateway-check.sh
+PreDown = ip route del default dev $vpn table gateway_pool metric $metric ; iptables -t nat -D POSTROUTING -o $vpn -j SNAT --to-source $ipv4 ; ip link delete $vpn ; freifunk-gateway-check.sh
 
 [Peer]
 PublicKey = $WG_PEER_PUBLIC_KEY
