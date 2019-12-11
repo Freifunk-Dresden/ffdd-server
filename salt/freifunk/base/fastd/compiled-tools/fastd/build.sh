25b1ee41880dc7de4dc12e798f66b596ab52c40a
creme <creme@envs.net>
2019-12-12 00:01:50 +0100

    optimize fastd build script

--- salt/freifunk/base/fastd/compiled-tools/fastd/build.sh
+++ salt/freifunk/base/fastd/compiled-tools/fastd/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ### This file managed by Salt, do not edit by hand! ###
 #
 # fastd + libuecc
@@ -14,16 +14,16 @@
 REPO_URL='https://github.com/NeoRaider/fastd.git'
 
 # fastd v17
-#fastd_rev=0358cbf937ee73447970546290a01f82c450dab9
-#libuecc_rev=bb4fcb93282ca2c3440294683c88e8d54a1278e0
+#fastd_rev='0358cbf937ee73447970546290a01f82c450dab9'
+#libuecc_rev='bb4fcb93282ca2c3440294683c88e8d54a1278e0'
 
 # fastd v18+ (devel)
-#fastd_rev=3995adf7882a43d4c7c206a1c5335e3fdbc9c529
-#libuecc_rev=7c9a6f6af088d0764e792cf849e553d7f55ff99e
+#fastd_rev='3995adf7882a43d4c7c206a1c5335e3fdbc9c529'
+#libuecc_rev='7c9a6f6af088d0764e792cf849e553d7f55ff99e'
 
 # master
-fastd_rev=8dc1ed3a1ee9af731205a7a4e167c1c2d1b3d819
-libuecc_rev=7c9a6f6af088d0764e792cf849e553d7f55ff99e
+fastd_rev='8dc1ed3a1ee9af731205a7a4e167c1c2d1b3d819'
+libuecc_rev='7c9a6f6af088d0764e792cf849e553d7f55ff99e'
 
 
 build_libuecc()
@@ -32,16 +32,16 @@ build_libuecc()
 	(
 		rm -rf libuecc
 		if [ -f libuecc-$libuecc_rev.tgz ]; then
-			tar xzf libuecc-$libuecc_rev.tgz
+			tar xzf libuecc-"$libuecc_rev".tgz
 		else
 			git clone "$REPO_URL"
-			git checkout $libuecc_rev
-			rev=$(git -C libuecc log -1 | sed -n '/^commit/s#commit ##p')
-			tar czf libuecc-$rev.tgz libuecc
+			git checkout "$libuecc_rev"
+			rev="$(git -C libuecc log -1 | sed -n '/^commit/s#commit ##p')"
+			tar czf libuecc-"$rev".tgz libuecc
 		fi
-		cd libuecc
+		cd libuecc || exit 1
 		mkdir build
-		cd build
+		cd build || exit 1
 		cmake ..
 		make
 		make install
@@ -80,20 +80,20 @@ build_fastd()
 
 		rm -rf fastd
 		if [ -f fastd-$fastd_rev.tgz ]; then
-			tar xzf fastd-$fastd_rev.tgz
+			tar xzf fastd-"$fastd_rev".tgz
 		else
 			git clone "$REPO_URL"
-			git checkout $fastd_rev
-			rev=$(git -C fastd log -1 | sed -n '/^commit/s#commit ##p')
-			tar czf fastd-$rev.tgz fastd
+			git checkout "$fastd_rev"
+			rev="$(git -C fastd log -1 | sed -n '/^commit/s#commit ##p')"
+			tar czf fastd-"$rev".tgz fastd
 		fi
 
 		patch --directory=fastd -p0 < urandom.patch
 
-		cd fastd
+		cd fastd || exit 1
 		mkdir build
-		cd build
-		cmake ../ $CMAKE_OPTIONS
+		cd build || exit 1
+		cmake ../ "$CMAKE_OPTIONS"
 		#cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -DENABLE_LIBSODIUM:BOOL=FALSE -DENABLE_LTO:BOOL=FALSE -DWITH_CAPABILITIES:BOOL=FALSE
 		make
 		strip src/fastd
