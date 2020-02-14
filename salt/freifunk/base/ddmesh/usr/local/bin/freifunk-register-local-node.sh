cb3ae31f19beb9f90a49fe4261d15cfc48a36be1
creme <creme@envs.net>
2020-02-14 17:20:38 +0100

    fix nvram_autosetup nodeid

--- salt/freifunk/base/ddmesh/usr/local/bin/freifunk-register-local-node.sh
+++ salt/freifunk/base/ddmesh/usr/local/bin/freifunk-register-local-node.sh
@@ -15,28 +15,30 @@ printf 'local node: [%s]\n' "$node"
 printf 'local key: [%s]\n\n' "$key"
 
 printf 'Try to register node [%s], key [%s]\n\n' "$node" "$key"
-n="$(wget -O - "http://register.freifunk-dresden.de/bot.php?node=$node&registerkey=$key" 2>/dev/null)"
+node_info="$(wget -O - "http://register.freifunk-dresden.de/bot.php?node=$node&registerkey=$key" 2>/dev/null)"
 
 
-cmd="$(echo "$n" | sed -n '/^OK/p;/^ERROR/p;/^INFO/p')"
+cmd="$(echo "$node_info" | sed -n '/^OK/p;/^ERROR/p;/^INFO/p')"
 case "$cmd" in
 
   OK*)
 	# shellcheck disable=SC2001
-	node="$(echo "$n" | sed 's#.*:\([0-9]\+\).*#\1#')"
-	printf 'node=%s\nupdated.\n' "$node"
+	node_id="$(echo "$node_info" | sed -n '/\"node\":/{s#^.*:##;p}')"
+	node_id="${node_id//\"/}"
+	printf 'node=%s\nupdated.\n' "$node_id"
+	printf '\nInfo:\n%s\n' "$node_info"
 	;;
 
   ERROR*)
-	printf '%s\n' "$n"
+	printf '%s\n' "$node_info"
 	;;
 
   INFO*)
-	printf '%s\n' "$n"
+	printf '%s\n' "$node_info"
 	;;
 
   *)
-	printf '%s\n' "$n"
+	printf '%s\n' "$node_info"
 	;;
 
 esac
