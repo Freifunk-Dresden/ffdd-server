8158b4cf4bed7de7381323c8ea1692bffc5a51b6
creme <creme@envs.net>
2020-02-17 11:14:56 +0100

    manuell_update: check nvram and nvram.conf is available

--- salt/freifunk/base/ddmesh/usr/local/bin/freifunk-manuell_update.sh
+++ salt/freifunk/base/ddmesh/usr/local/bin/freifunk-manuell_update.sh
@@ -6,12 +6,13 @@ INSTALL_DIR='/srv/ffdd-server'
 if [ "$(id -u)" -ne 0 ]; then printf 'Please run as root!\n'; exit 1 ; fi
 
 
-CUSTOM_REPO_URL="$(nvram get freifunk_repo)"
-[ -n "$CUSTOM_REPO_URL" ] && [ "$CUSTOM_REPO_URL" != "$REPO_URL" ] && REPO_URL="$CUSTOM_REPO_URL"
+if [ -f /usr/local/bin/nvram ] && [ -f /etc/nvram.conf ]; then
+	CUSTOM_REPO_URL="$(nvram get freifunk_repo)"
+	[ -n "$CUSTOM_REPO_URL" ] && [ "$CUSTOM_REPO_URL" != "$REPO_URL" ] && REPO_URL="$CUSTOM_REPO_URL"
 
-CUSTOM_REV="$(nvram get branch)"
-[ -n "$CUSTOM_REV" ] && [ "$CUSTOM_REV" != "$REV" ] && REV="$CUSTOM_REV"
-
+	CUSTOM_REV="$(nvram get branch)"
+	[ -n "$CUSTOM_REV" ] && [ "$CUSTOM_REV" != "$REV" ] && REV="$CUSTOM_REV"
+fi
 
 [ -n "$INSTALL_DIR" ] && rm -rf "$INSTALL_DIR"
 git clone "$REPO_URL" "$INSTALL_DIR"
