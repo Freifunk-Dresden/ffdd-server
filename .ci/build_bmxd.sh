3ba50ac92c57e1deed5bdc1205ae9e62a0954af9
creme <creme@envs.net>
2020-05-28 11:29:22 +0200

    add REVISION for package name

--- .ci/build_bmxd.sh
+++ .ci/build_bmxd.sh
@@ -8,6 +8,7 @@ git checkout latest_server
 
 ARCH='amd64'
 VERSION="$(grep -R 'SOURCE_VERSION' batman.h | awk '/SOURCE_VERSION/ {print $3}' | sed -e 's/^"//' -e 's/"$//')"
+REVISION="$(cat /tmp/bmx_revision)"
 
 chmod 755 DEBIAN
 chmod 555 DEBIAN/*
@@ -21,8 +22,9 @@ cp -RPfv DEBIAN OUT/
 cd OUT
 sed -i "s/ARCH/$ARCH/g" DEBIAN/control
 sed -i "s/VERSION/$VERSION/g" DEBIAN/control
+sed -i "s/REVISION/$REVISION/g" DEBIAN/control
 md5sum "$(find . -type f | grep -v '^[.]/DEBIAN/')" > DEBIAN/md5sums
 
-dpkg-deb --build ./ ../../bmxd-"$VERSION"_"$ARCH".deb
+dpkg-deb --build ./ ../../bmxd-"$VERSION"-"$REVISION"_"$ARCH".deb
 
 exit 0
