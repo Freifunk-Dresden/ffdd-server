25ee6640b61d2b12d318ec5b18999bcf60f20bcd
creme <creme@envs.net>
2022-10-18 13:06:03 +0200

    drone-ci fix reference

--- .ci/build_uci.sh
+++ .ci/build_uci.sh
@@ -6,14 +6,14 @@ test ! -d build && mkdir build ; cd build
 git clone https://git.openwrt.org/project/libubox.git
 cd libubox || exit 1
 #git checkout 7da66430de3fc235bfc6ebb0b85fb90ea246138d
-git checkout 7e47fe657d6ab889667a77f6c03ace62c4d2d077
+git checkout ea56013409d5823001b47a9bba6f74055a6d76a5
 cd ..
 
 # LIBUCI und UCI
 git clone https://git.openwrt.org/project/uci.git
 cd uci || exit 1
 #git checkout ec8d3233948603485e1b97384113fac9f1bab5d6
-git checkout f0e6e9fe3ea3d0dbe33785cce506341d856c54cf
+git checkout f49a2fdc4fb4a3bc95c228ade38332685197210f
 cd ..
 
 
