02d75e44c4e79f999d0f64a17513065bd2943edf
creme <creme@envs.net>
2023-07-26 13:15:56 +0200

    freifunk-uci_autosetup.sh: remove old fastd part

--- salt/freifunk/base/ddmesh/usr/local/bin/freifunk-uci_autosetup.sh
+++ salt/freifunk/base/ddmesh/usr/local/bin/freifunk-uci_autosetup.sh
@@ -10,8 +10,7 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 ddmesh_node="$(uci -qX get ffdd.sys.ddmesh_node)"
 ddmesh_key="$(uci -qX get dffdd.sys.ddmesh_registerkey)"
 
-if [ -z "$ddmesh_key" ] || [ "$ddmesh_key" = '-' ] || \
-	[ -z "$fastd_secret" ] || [ "$fastd_secret" = '-' ]; then
+if [ -z "$ddmesh_key" ] || [ "$ddmesh_key" = '-' ]; then
 
 		# set ddmesh_registerkey
 		ddmesh_key="$(ip link | sha256sum | sed 's#\(..\)#\1:#g;s#[ :-]*$##')"
