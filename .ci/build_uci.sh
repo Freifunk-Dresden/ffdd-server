067fa10d06aa51044153b5cee314a816eed6daef
creme <creme@envs.net>
2024-01-04 05:56:35 +0100

    S52batmand: use PREFERED_GATEWAY if configured

--- .ci/build_uci.sh
+++ .ci/build_uci.sh
@@ -6,23 +6,20 @@ test ! -d build && mkdir build ; cd build
 git clone https://git.openwrt.org/project/libubox.git
 cd libubox || exit 1
 git checkout ea56013409d5823001b47a9bba6f74055a6d76a5
-cd ..
-mkdir BUILD-libubox
+cd .. ; mkdir BUILD-libubox
 
 # UCI
 git clone https://git.openwrt.org/project/uci.git
 cd uci || exit 1
 git checkout f84f49f00fb70364f58b4cce72f1796a7190d370
-cd ..
-mkdir BUILD-uci
+cd .. ; mkdir BUILD-uci
 
 # UBOX
 git clone https://git.openwrt.org/project/ubox.git
 # needs to be include udebug for master branch
 cd ubox || exit 1
 git checkout 4c7b720b9c63b826fb9404e454ae54f2ef5649d5
-cd ..
-mkdir BUILD-ubox
+cd .. ; mkdir BUILD-ubox
 
 # UBUS
 git clone https://git.openwrt.org/project/ubus.git
